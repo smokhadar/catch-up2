9969fdc05f72f742a52cdd2757a75bd5ae1ffec1
Samimi <smokhadar@gmail.com>
2023-08-21 21:13:52 -0700

    added comments to post model

--- server/models/Post.js
+++ server/models/Post.js
@@ -19,7 +19,25 @@ const postSchema = new Schema({
         default: Date.now,
         get: (timestamp) => dateFormat(timestamp),
     },
-    // comments: [comments]
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280,
+              },
+              commentAuthor: {
+                type: String,
+                required: true,
+              },
+              createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+            },
+        },
+    ],
 });
 
 const Post = model('Post', postSchema);
