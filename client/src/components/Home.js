fd8df0f917b306a806a856b9f259ebfc083b26ba
deandradiedrick <dadiedrick@gmail.com>
2023-08-24 20:38:41 -0700

    home page
    added dependency

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,38 +1,68 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { Grid, Transition, Input, Menu, Header, Segment, Container} from 'semantic-ui-react';
 
-// // import components
 
-// export function Home() {
-//     return (
-//         <div>
-//             <Profile />
-//             <AllPosts />
-//             <Friends />
-//             <Chat />
-//         </div>
-//     );
-// }
-//import { div, useMediaQuery } from "@mui/material";
-//import { useSelector } from "react-redux";
+
 
 
 const HomePage = () => {
-  //const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-  //const { _id, picturePath } = useSelector((state) => state.user);
-
+  
+  //render() {
+  const  {color}  = 'red';
   return (
-    <div>
-     <header> Catch Up Home Page </header>
-     <nav>
-    <a href="#">Notifications </a>
-    <a href="#">Messages </a>
-    <a href="#">Friends </a>
-    <input type="search" id="search" placeholder='search for friends'></input>
-   
-   </nav>
-   </div>
-   
+  
+
+    //  <Grid columns={3}>
+         <div> 
+    <Segment>
+     <Header as='h1' textAlign='center'>Welcome to CatchUp 2.0</Header>
+   </Segment>
+ </div>,
+      //</Grid><Grid.Row className="page-title">
+      
+        <Menu color='olive' inverted widths>
+        <Menu.Item
+          name='home'
+          // active={activeItem === 'home'}
+          // onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='messages'
+          // active={activeItem === 'messages'}
+          // onClick={this.handleItemClick}
+        />
+        <Menu.Item
+          name='friends'
+          // active={activeItem === 'friends'}
+          // onClick={this.handleItemClick}
+        />
+         <Menu.Item
+         />
+        <Menu.Menu position='center'>
+          <Menu.Item header>
+          <Header >Welcome to CatchUp 2.0</Header>
+          </Menu.Item>
+          <Menu.Item   
+         />
+        </Menu.Menu>
+        <Menu.Menu position='right'>
+          <Menu.Item>
+            <Input icon='search' placeholder='Search...' />
+          </Menu.Item>
+          <Menu.Item
+            name='logout'
+            // active={activeItem === 'logout'}
+            // onClick={this.handleItemClick}
+          />
+        </Menu.Menu>
+      </Menu>
+     
+    
+         
   );
+ // }
+ 
 };
 
 export default HomePage;
