ab90f3a6f0fc2d8bfd257cd7818553b035adb9bd
Samimi <smokhadar@gmail.com>
2023-08-24 21:26:21 -0700

    connected client to backend

--- client/src/components/NewPost.js
+++ client/src/components/NewPost.js
@@ -1,18 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {useMutation, useQuery } from '@apollo/client';
+import {CREATE_POST} from '../utils/mutations';
 
 export const NewPost = () => {
-    const createNewPost = () => {
-        // create new post mutation
+    const [formState, setFormState] = useState({
+        postBody: '',
+        username: 'test'
+    })
+    const [createPost, { error }] = useMutation(CREATE_POST);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    }
+    const handleFormSubmit = async(event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await createPost({
+                variables: { postText: formState.postBody, postAuthor: formState.username },
+            });
+
+            console.log('post successfully created');
+        } catch(e) {
+            console.error(e);
+        }
     }
 
     return (
         <div>
-            <form className="newpost">
+            <form onSubmit={handleFormSubmit} className="newpost">
                 <label>What's on your mind?</label>
-                <textarea type="text" rows="3"></textarea>
+                <textarea 
+                name="postBody"
+                value={formState.postBody} 
+                onChange={handleChange}
+                type="text" rows="3"></textarea>
                 {/* upload image option? */}
-                <button onSubmit={createNewPost}>Post</button>
+                <button>Post</button>
             </form>
+
+            {error && (
+                <div>
+                    {error.message}
+                </div>
+            )}
         </div>
     );
 }
